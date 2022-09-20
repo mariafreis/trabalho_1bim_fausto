be2a095b772c73e2beaac9d3b49ae149365af412
FlutLab.io <>
2022-09-20 04:27:28 +0000

    finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,90 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Construindo layouts"),
+          ),
+          body: corpo()),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+Widget corpo() {
+  return Column(
+    children: [
+      Expanded(flex: 1, child: icones()),
+      Expanded(flex: 5, child: imagem()),
+      Expanded(flex: 4, child: caixa())
+    ],
+  );
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+Widget icones() {
+  return Row(
+    children: const <Widget>[
+      Expanded(child: Icon(Icons.home)),
+      Expanded(child: Icon(Icons.access_time)),
+      Expanded(child: Icon(Icons.account_balance)),
+      Expanded(child: Icon(Icons.add_a_photo)),
+    ],
+  );
+}
+
+Widget imagem() {
+  //para exibir uma imagem da internet
+  return Image.network(
+    'https://www.unifacef.com.br/wp-content/uploads/2016/02/logo-300x70.png',
+  );
+}
+
+Widget caixa() {
+  return Container(
+      margin: const EdgeInsets.all(5.0),
+      padding: const EdgeInsets.all(10.0),
+      alignment: Alignment.topCenter,
+      width: 400,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Color(0xffa3ba78),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      child: text());
+}
+
+Widget text() {
+  return Column(
+    children: [
+      Expanded(
+        flex: 16,
+        child: const Text("Exemplo de layout Flutter",
+            style: TextStyle(fontSize: 25)),
       ),
-    );
-  }
+      Expanded(
+        flex: 2,
+        child: textvazio(),
+      )
+    ],
+  );
+}
+
+Widget textvazio() {
+  return Row(
+    children: [
+      Expanded(
+        flex: 10,
+        child: const Text(""),
+      ),
+      Expanded(
+        flex: 0,
+        child: const Text("Desenvolvido por Maria F. M. Reis",
+            style: TextStyle(
+                fontSize: 16,
+                color: Color(0xff0940a8),
+                fontWeight: FontWeight.bold)),
+      )
+    ],
+  );
 }
